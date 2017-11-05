9cee32a68b0b83f5857751b622a746a7c298dce5
Julie David <julie.angela.david@gmail.com>
2017-11-05 11:45:16 -0500

    install app core, add view to return HTTP response, added URL to run on server root URL

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def greeting_view(request):
+    return HttpResponse("Hello World")
--- todos/urls.py
+++ todos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import greeting_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', greeting_view),
 ]
